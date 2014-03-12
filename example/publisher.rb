b5181bb027fbcc4d1e12f088c3590b7306fe5c46
John Van Enk <vanenkj@gmail.com>
2014-03-12 14:33:55 -0400

    Document the examples.

--- example/publisher.rb
+++ example/publisher.rb
@@ -1,7 +1,14 @@
 require 'noam-lemma'
-require 'socket'
 
-# expects a Noam server to be running
+# This is an example of a Ruby Lemma that publishes messages. It expects a Noam
+# server to be running. It also expects that the nome-lemma.rb file is in the
+# search path. If you run this example from the project root, the following
+# command should work:
+#
+#   ruby -Ilib example/publisher.rb
+#
+# This example _will not_ work on the same machine running the Nome server as
+# both programs need to bind to UDP port 1030.
 
 publisher = Noam::Lemma.new(
   'example-publisher',
@@ -10,23 +17,36 @@ publisher = Noam::Lemma.new(
   [],
   ["e1","e2"])
 
+# Using the `start` method asks the Lemma to proactively try and discover a
+# server to connect to on the local network. Once the server is discovered, it
+# will connect and send a Nome 'register' message. When `start` returns, the
+# Lemma is ready to send events.
 publisher.start
 
 seq = 0
 loop do
+  # This method block picks an event to send. It's randomized a bit so that we
+  # can see things change over time.
   e = if 0.5 < rand()
         "e1"
       else
         "e2"
       end
+
+  # Next, package the event sequence and the current time into a value.
   v = {"seq" => seq, "time" => Time.now.to_s}
 
+  # Attempt to play the chosen event with the value. Note, the event is either
+  # "e1" or "e2" based on how rand() returned.
   unless publisher.play(e, v)
+    # If `play` returns false, we're unable to play the message likely because
+    # the socket has closed. The connection would have to be restarted.
     puts "Done"
     break
   end
   puts "Wrote: #{e} -> #{v.inspect}"
 
   seq += 1
-  sleep(1 + rand())
+  # Sleep for a while so that we don't bog down the network.
+  sleep(1)
 end
--- example/subscriber.rb
+++ example/subscriber.rb
@@ -1,7 +1,15 @@
 require 'noam-lemma'
-require 'socket'
 
-# expects a Noam server to be running
+# This is an example of a Ruby Lemma that subscribes to messages. It expects a
+# Nome server to be running. It also expects that the nome-lemma.rb file is in
+# the search path. If you run this example from the project root, the following
+# command should work:
+#
+#   ruby -Ilib example/subscriber.rb
+#
+# This example _will not_ work on the same machine running the Nome server as
+# both programs need to bind to UDP port 1030.
+
 subscriber = Noam::Lemma.new(
   'example-subscriber',
   'ruby-script',
@@ -9,11 +17,20 @@ subscriber = Noam::Lemma.new(
   ["e1","e2"],
   [])
 
+# Using the `start` method asks the Lemma to proactively try and discover a
+# server to connect to on the local network. Once the server is discovered, it
+# will connect and send a Nome 'register' message. When `start` returns, the
+# Lemma is ready to receive events.
 subscriber.start
 
 loop do
+  # The `listen` method will return an Event object once one is received by the
+  # Lemma. Until an event is heard, the `listen` method blocks.
   m = subscriber.listen
 
+  # There's one special value that's returned from `listen`: the `:cancelled`
+  # symbol. If this shows up, it means some one else has called the `stop`
+  # method on the Lemma.
   if :cancelled == m
     puts "Done"
     break
